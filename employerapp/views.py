f460fd8776473c04081e9018a3bef57cf806b16a
akrus <ruslan.akm@gmail.com>
2021-03-08 22:34:01 +0300

    * Start build jobseekerapp

--- employerapp/views.py
+++ employerapp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from authapp.models import Employer
+from authapp.models import Employer, IndustryType
 
 def employer_cabinet(request):
     title = 'Личный кабинет работодателя'
     current_user = request.user.id
     employer_data = Employer.objects.get(user_id=current_user)
-    content = {'title': title, 'employer': employer_data}
+    industry_type = IndustryType.objects.get(id=employer_data.industry_type_id)
+    content = {'title': title, 'employer': employer_data, 'industry_type':industry_type.descx}
     return render(request, 'employerapp/employer_cabinet.html', content)
+
